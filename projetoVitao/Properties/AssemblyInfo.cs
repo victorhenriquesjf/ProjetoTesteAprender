0b952530472c9b10fa6137c726ca0f9458799f1d
Victor HAlmeida.EXT <Victor.HAlmeida.EXT@thomsonreuters.com>
2020-02-20 10:46:31 -0300

    first commit

---
+++ projetoVitao/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("projetoVitao")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("projetoVitao")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49fef91e-1ae4-49f3-b900-082ae5463845")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ projetoVitao/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace projetoVitao
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver(); // criando referencia o meu navegador
+
+            driver.Navigate().GoToUrl("http://www.google.com.br"); // para navegar
+
+            //IWebElement element = driver.FindElement(By.Name("q"));
+
+            IWebElement element = driver.FindElement(By.CssSelector("input.gLFyf.gsfi"));
+            //
+
+            element.SendKeys("botafogo campeão");
+
+
+            driver.Close();
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            IWebDriver driver = new ChromeDriver(); // criando referencia o meu navegador
+
+            driver.Navigate().GoToUrl("http://www.google.com.br"); // para navegar
+
+            //IWebElement element = driver.FindElement(By.Name("q"));
+
+            IWebElement element = driver.FindElement(By.CssSelector("input.gLFyf.gsfi"));
+            //
+
+            element.SendKeys("botafogo campeão");
+
+
+            driver.Close();
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            IWebDriver driver = new ChromeDriver(); // criando referencia o meu navegador
+
+            driver.Navigate().GoToUrl("http://www.google.com.br"); // para navegar
+
+            //IWebElement element = driver.FindElement(By.Name("q"));
+
+            IWebElement element = driver.FindElement(By.CssSelector("input.gLFyf.gsfi"));
+            //
+
+            element.SendKeys("botafogo campeão");
+
+
+            driver.Close();
+
+        }
+
+    }
+}
